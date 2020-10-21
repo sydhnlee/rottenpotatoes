4b84bd8376551f54e66ef9100dfc0d30f8789fac
Sydney Lee <sydhnlee@berkeley.edu>
2020-10-21 07:00:16 +0000

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ActiveRecord::Base
+  @@all_ratings = ['G', 'PG', 'PG-13', 'R']
+  
+  def self.all_ratings
+    @@all_ratings
+  end
+  
+  def self.with_ratings(ratings)
+    if ratings.empty?
+      self.all
+    else
+      self.where(rating: ratings)
+    end
+  end
 end
